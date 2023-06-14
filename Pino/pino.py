39e4a1444b474df28e33b9b2c0815cdacbef46c9
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-06-14 11:57:04 -0600

    Start Pino

---
+++ Pino/pino.py
@@ -0,0 +1,8 @@
+'''
+Andres Quezada
+Prueba LOMAX - Ejercicio 2
+14/06/23
+Realiza un programa el cual dibuje un pino en consola dada una altura para el
+árbol. Usar la imagen de referencia. En la imágen el árbol tiene altura 10.
+'''
+
