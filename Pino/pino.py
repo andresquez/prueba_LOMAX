540cd968238a68df633f45db466324048acf1989
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-06-14 12:13:03 -0600

    Finish Pino

--- Pino/pino.py
+++ Pino/pino.py
@@ -6,3 +6,13 @@ Realiza un programa el cual dibuje un pino en consola dada una altura para el
 árbol. Usar la imagen de referencia. En la imágen el árbol tiene altura 10.
 '''
 
+def draw_pine_tree(height):
+    for i in range(1, height+1):
+        print(f"{' ' * (height - i)}{'* ' * i }\n")
+
+        
+    print(f"{' ' * (height - 1)}*\n{' ' * (height - 1)}*")
+          
+
+if __name__ == "__main__":
+    draw_pine_tree(10)
